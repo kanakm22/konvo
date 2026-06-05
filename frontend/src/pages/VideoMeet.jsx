60fe97bfcec626c4967972df834fbbff46c134ce
Kanak Maurya <kanakmaurya22@gmail.com>
2026-06-05 08:23:58 +0530

    implement screen share

--- frontend/src/pages/VideoMeet.jsx
+++ frontend/src/pages/VideoMeet.jsx
@@ -434,6 +434,70 @@ function VideoMeet() {
     }
   };
 
+  let getDisplayMediaSuccess = (stream) =>{
+    try{
+      window.localStream.getTracks().forEach((track) => track.stop());
+    }catch(e){
+      console.log(e);
+    }
+
+    window.localStream = stream;
+    localVideoRef.current.srcObject = stream;
+
+    for(let id in connections) {
+      if(id === socketId.current) continue;
+
+      connections[id].addStream(window.localStream)
+      connections[id].createOffer()
+      .then((description) =>{
+        connections[id].setLocalDescription(description)
+        .then( () =>{
+          socketRef.current.emit("signal",id, JSON.stringify({"sdp": connections[id].localDescription}))
+        })
+        .catch(e => console.log(e))
+      })
+    }
+    stream.getTracks().forEach(track => track.onended = () => {
+      setScreen(false);
+
+      try{
+        let tracks = localVideoRef.current.srcObject.getTracks();
+        tracks.forEach(track => track.stop())
+      }catch(e){
+        console.log(e);
+      }
+       let blacksilence = (...args) => new MediaStream([black(...args), silence()])
+      window.localStream = blacksilence();
+      localVideoRef.current.srcObject = window.localStream;
+
+      getUserMedia();
+
+    })
+    
+
+  }
+
+  let getDisplayMedia = () =>{
+    if(screen){
+      if(navigator.mediaDevices.getDisplayMedia){
+        navigator.mediaDevices.getDisplayMedia({video: true, audio: true})
+        .then(getDisplayMediaSuccess)
+        .then((screen = { }))
+        .catch((e) => console.log(e))
+      }
+    }
+  }
+
+  useEffect(() =>{
+    if(screen !== undefined){
+      getDisplayMedia();
+    }
+  }, [screen])
+
+  let handleScreen = () =>{
+    setScreen(!screen);
+  }
+
 
   return (
     <div>
@@ -471,7 +535,7 @@ function VideoMeet() {
             </IconButton>
 
             {screenAvailable === true ?
-              <IconButton style={{ color: "white" }}>
+              <IconButton onClick={handleScreen} style={{ color: "white" }}>
                 {screen === true ? <ScreenShareIcon /> : <StopScreenShareIcon />}
               </IconButton> : <></>}
 
